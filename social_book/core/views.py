3d54a16fe299dec03652ff2ded05be9f10df761b
nanjeribrian <brian@castynet.africa>
2023-09-05 18:49:57 +0300

    feat:made static files and profile model

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method=='POST':
+        return render(request, 'signup.html')
