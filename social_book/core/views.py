797fbc69973b2483173d3478c480584bd3844de8
nanjeribrian <brian@castynet.africa>
2023-09-18 11:42:41 +0300

    feat:like post and profile page

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post,LikePost
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -27,6 +27,43 @@ def upload(request):
 
     else:
         return redirect('/')
+
+@login_required(login_url='signin')
+def like_post(request):
+    username=request.user.username
+    post_id=request.GET.get('post_id')
+    
+    post=Post.objects.get(id=post_id)
+    print('post', post)
+    like_filter=LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter==None:
+        new_like=LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes=post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes=post.no_of_likes-1
+        post.save()
+        return redirect('/')
+
+@login_required(login_url='signin')
+def profile(request, pk):
+    user_object=User.objects.get(username=pk)
+    user_profile=Profile.objects.get(user=user_object)
+    user_post=Post.objects.filter(user=pk)
+    user_post_length=len(user_post)
+    context={
+        'user_object':user_object,
+        'user_profile':user_profile,
+        'user_posts':user_post,
+        'user_post_length':user_post_length 
+    }
+    return render(request, 'profile.html', context)
+    
+    
 @login_required(login_url='signin')
 def settings(request):
     user_profile=Profile.objects.get(user=request.user)
